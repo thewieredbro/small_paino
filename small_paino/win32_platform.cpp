382cce77050f2ca94f04952c177d4a7c8d485440
thewieredbro <162340081+thewieredbro@users.noreply.github.com>
2024-11-11 22:11:36 +0200

    Reapply "Add project files."
    This reverts commit 1c18c7030439f2b3dc01548b768b62259a13d0fb.

---
+++ small_paino/win32_platform.cpp
@@ -0,0 +1,39 @@
+#include <Windows.h>
+
+bool running = true;
+
+LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+    LRESULT result = 0;
+    switch (uMsg) {
+    case WM_CLOSE:
+    case WM_DESTROY: {
+        running = false;
+    } break;
+
+    default: {
+        result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    WNDCLASS window_class = {};
+    window_class.style = CS_HREDRAW | CS_VREDRAW;
+    window_class.lpszClassName = "small piano";
+    window_class.lpfnWndProc = window_callback;
+
+    RegisterClass(&window_class);
+
+    HWND window = CreateWindow(window_class.lpszClassName, "paino", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1600, 900, 0, 0, hInstance, 0);
+
+    while (running) {
+        MSG message;
+        while (PeekMessageA(&message, window, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&message);
+            DispatchMessage(&message);
+        }
+    }
+}
